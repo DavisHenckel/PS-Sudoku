2ea61c54b54c13fd54310c931bdef7e069010000
Davis Henckel <51898571+DavisHenckel@users.noreply.github.com>
2022-04-07 22:01:31 -0700

    Build CI file

---
+++ build.ps1
@@ -0,0 +1,43 @@
+param (
+    [ValidateSet("Release", "debug")]$Configuration = "debug",
+    [Parameter(Mandatory=$false)][String]$NugetAPIKey,
+    [Parameter(Mandatory=$false)][Switch]$ExportAlias
+)
+
+task Init {
+    Write-Verbose -Message "Initializing Module PSScriptAnalyzer"
+    if (-not(Get-Module -Name PSScriptAnalyzer -ListAvailable)){
+        Write-Warning "Module 'PSScriptAnalyzer' is missing or out of date. Installing module now."
+        Install-Module -Name PSScriptAnalyzer -Scope CurrentUser -Force
+    }
+
+    Write-Verbose -Message "Initializing Module Pester"
+    if (-not(Get-Module -Name Pester -ListAvailable)){
+        Write-Warning "Module 'Pester' is missing or out of date. Installing module now."
+        Install-Module -Name Pester -Scope CurrentUser -Force
+    }
+
+    Write-Verbose -Message "Initializing PowerShellGet"
+    if (-not(Get-Module -Name PowerShellGet -ListAvailable)){
+        Write-Warning "Module 'PowerShellGet' is missing or out of date. Installing module now."
+        Install-Module -Name PowerShellGet -Scope CurrentUser -Force
+    }
+}
+
+task Test {
+    try {
+        Write-Verbose -Message "Running PSScriptAnalyzer on Public functions"
+        Invoke-ScriptAnalyzer ".\Source\Public" -Recurse
+        Write-Verbose -Message "Running PSScriptAnalyzer on Private functions"
+        Invoke-ScriptAnalyzer ".\Source\Private" -Recurse
+    }
+    catch {
+        throw "Couldn't run Script Analyzer"
+    }
+
+    Write-Verbose -Message "Running Pester Tests"
+    $Results = Invoke-Pester -Script ".\Tests\UnitTests\*.ps1" -OutputFormat NUnitXml -OutputFile ".\Tests\UnitTests\TestResults.xml"
+    if($Results.FailedCount -gt 0){
+        throw "$($Results.FailedCount) Tests failed"
+    }
+}
