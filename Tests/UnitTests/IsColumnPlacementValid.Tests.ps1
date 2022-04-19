e6990ffed73c504dff1483e35c37d1a1dd5f2adb
Davis Henckel <51898571+DavisHenckel@users.noreply.github.com>
2022-04-19 08:13:15 -0700

    Update to Pester 5

--- Tests/UnitTests/IsColumnPlacementValid.Tests.ps1
+++ Tests/UnitTests/IsColumnPlacementValid.Tests.ps1
@@ -1,28 +1,30 @@
 Describe 'IsColumnPlacementValid' {
-    $SudokuGrid = GenerateGrid
+    BeforeAll {
+        $SudokuGrid = GenerateGrid
+    }
     It 'Ensure empty grid has valid number placement in all columns'{
         For ($i = 0; $i -lt 9; $i++) {
-            IsColumnPlacementValid $SudokuGrid ($i+1) 1 | Should be $true
+            IsColumnPlacementValid $SudokuGrid ($i+1) 1 | should -be $true
         }
     }
     It 'Ensure non-empty grid with invalid number placement is invalid'{
         $SudokuGrid[0][0] = 1
-        IsColumnPlacementValid $SudokuGrid 1 1 | Should be $false
+        IsColumnPlacementValid $SudokuGrid 1 1 | should -be $false
     }
     It 'Ensure non-empty grid with valid placement is valid'{
         $SudokuGrid[0][0] = 1
-        IsColumnPlacementValid $SudokuGrid 1 9 | Should be $true
+        IsColumnPlacementValid $SudokuGrid 1 9 | should -be $true
     }
     It 'Ensure out of upper bounds column placement returns error'{
-        { IsColumnPlacementValid $SudokuGrid 11 2 } | Should Throw 
+        { IsColumnPlacementValid $SudokuGrid 11 2 } | Should -Throw 
     }
     It 'Ensure out of lower bounds column placement returns error'{
-        { IsColumnPlacementValid $SudokuGrid 0 2 } | Should Throw 
+        { IsColumnPlacementValid $SudokuGrid 0 2 } | Should -Throw 
     }
     It 'Ensure out of lower bounds column placement returns error'{
-        { IsColumnPlacementValid $SudokuGrid 1 0 } | Should Throw 
+        { IsColumnPlacementValid $SudokuGrid 1 0 } | Should -Throw 
     }
     It 'Ensure out of upper bounds number to place returns error'{
-        { IsColumnPlacementValid $SudokuGrid 11 20 } | Should Throw 
+        { IsColumnPlacementValid $SudokuGrid 11 20 } | Should -Throw 
     }
 }
