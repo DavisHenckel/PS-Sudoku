abb908d51f0698618c86c20fd46a3b4e65aa5f3b
Davis Henckel <51898571+DavisHenckel@users.noreply.github.com>
2022-04-23 07:09:37 -0700

    Improve install script

--- LocalModuleInstall.ps1
+++ LocalModuleInstall.ps1
@@ -16,9 +16,19 @@ Function DetermineCorrectModulePath {
     }
     return $PossiblePaths[0] #if only 1 path and not programfiles.
 }
-
-$InstallPath = DetermineCorrectModulePath
-$InstallPath = $InstallPath + "\PowerShell-CICD"
+$InstallPath = $null
+if ($IsWindows) {
+    $InstallPath = "C:\Users\runneradmin\Documents\PowerShell\Modules\PowerShell-CICD"
+}
+elseif ($IsLinux) {
+    $InstallPath = "/home/runner/.local/share/powershell/Modules/PowerShell-CICD"
+}
+elseif ($IsMacOS) {
+    $InstallPath = "/Users/runner/.local/share/powershell/Modules/PowerShell-CICD"
+}
+else {
+    $InstallPath = DetermineCorrectModulePath
+}
 if (Test-Path $InstallPath) {
     Write-Host "Uninstalling pre-existing module versions..." -ForegroundColor Yellow
     Remove-Item -Recurse -Force $InstallPath
@@ -27,7 +37,7 @@ Write-Host "Installing PowerShell-CICD module version $Version`..." -ForegroundC
 New-Item -ItemType Directory "$InstallPath\$Version" | Out-Null
 Copy-Item "$($PSScriptRoot)\*" -Exclude "*git*" -Recurse -Destination "$InstallPath\$Version"
 Write-Host "Ensuring module is properly installed..." -ForegroundColor Yellow
-Import-Module PowerShell-CICD
+Import-Module PowerShell-CICD -Force
 if (-not (Get-Command -Module PowerShell-CICD)){
     Write-Host "Module not installed properly. Manually move the folder to $InstallPath`\`$Version" -ForegroundColor Red
 }
