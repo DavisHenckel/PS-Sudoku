c2ac61da5437b6f4fe602780688af71f14b83d9b
Davis Henckel <henckeld@oregonstate.edu>
2022-05-09 20:39:35 -0700

    Created function for determining valid move

--- Private/GetSudokuMove.ps1
+++ Private/GetSudokuMove.ps1
@@ -12,3 +12,38 @@
 .OUTPUTS
     Returns a 2d Tuple, with [0] being the number to place and [1] being the row, column to place at.
 #>
+Function GetSudokuMove {
+    param(
+        [parameter(Mandatory=$true)]
+        [System.Object]$SudokuGrid
+    )
+    while($true) {
+        $Row = Read-Host -Prompt "Enter row placement(1-9)"
+        $Column = Read-Host -Prompt "Enter column placement(1-9)"
+        $NumToPlace = Read-Host -Prompt "Enter the nunber to place (1-9)"
+        $RowValid = IsRowPlacementValid -SudokuGrid $SudokuGrid -Row $Row -Number $NumToPlace
+        $ColValid = IsColumnPlacementValid -SudokuGrid $SudokuGrid -Column $Column -Number $NumToPlace
+        $SubgridValid = IsSubgridPlacementValid -SudokuGrid $SudokuGrid -Row $Row -Column $Column -Number $NumToPlace
+        Write-Host "Row Placement: " -ForegroundColor Yellow -NoNewline
+        if ($RowValid) {
+            Write-Host "Valid!" -ForegroundColor Green
+        }
+        else {
+            Write-Host "Invalid!" -ForegroundColor Red
+        }
+        Write-Host "Column Placement: " -ForegroundColor Yellow  -NoNewline
+        if ($ColValid) {
+            Write-Host "Valid!" -ForegroundColor Green
+        }
+        else {
+            Write-Host "Invalid!" -ForegroundColor Red
+        }
+        Write-Host "Subgrid Placement: " -ForegroundColor Yellow  -NoNewline
+        if ($SubgridValid) {
+            Write-Host "Valid!" -ForegroundColor Green
+        }
+        else {
+            Write-Host "Invalid!" -ForegroundColor Red
+        }
+    }    
+}
