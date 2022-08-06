0ae5487324eb5376f179b69feeecf9e970e4a86c
LiquidDeath911 <35089748+LiquidDeath911@users.noreply.github.com>
2022-08-06 18:01:38 -0500

    Style Changes

--- Tests/AcceptanceTests/SolveSudoku.Tests.ps1
+++ Tests/AcceptanceTests/SolveSudoku.Tests.ps1
@@ -2,90 +2,90 @@ Describe "SolveSudoku" {
     It "Verifying Easy board solvable" {
         $Count = 0
         $Solved = $false
-        while($true) {
+        While($true) {
             $Grid = GenerateGrid -Difficulty "Easy"
             $Timer =  [system.diagnostics.stopwatch]::StartNew()
             $Solved = SolveSudoku -SudokuGrid $Grid -StopWatch $Timer
-            if ($Count -eq 25) {
-                break
+            If ($Count -eq 25) {
+                Break
             }
-            if (-not $Solved) {
-                continue
+            If (-not $Solved) {
+                Continue
                 $Count += 1
             }
-            break
+            Break
         }
         $Solved | should -be $true
     }
     It "Verifying Medium board solvable" {
         $Count = 0
         $Solved = $false
-        while($true) {
+        While($true) {
             $Grid = GenerateGrid -Difficulty "Medium"
             $Timer =  [system.diagnostics.stopwatch]::StartNew()
             $Solved = SolveSudoku -SudokuGrid $Grid -StopWatch $Timer
-            if ($Count -eq 25) {
-                break
+            If ($Count -eq 25) {
+                Break
             }
-            if (-not $Solved) {
-                continue
+            If (-not $Solved) {
+                Continue
                 $Count += 1
             }
-            break
+            Break
         }
         $Solved | should -be $true
     }
     It "Verifying Hard board solvable" {
         $Count = 0
         $Solved = $false
-        while($true) {
+        While($true) {
             $Grid = GenerateGrid -Difficulty "Hard"
             $Timer =  [system.diagnostics.stopwatch]::StartNew()
             $Solved = SolveSudoku -SudokuGrid $Grid -StopWatch $Timer
-            if ($Count -eq 25) {
-                break
+            If ($Count -eq 25) {
+                Break
             }
-            if (-not $Solved) {
-                continue
+            If (-not $Solved) {
+                Continue
                 $Count += 1
             }
-            break
+            Break
         }
         $Solved | should -be $true
     }
     It "Verifying Expert board solvable" {
         $Count = 0
         $Solved = $false
-        while($true) {
+        While($true) {
             $Grid = GenerateGrid -Difficulty "Expert"
             $Timer =  [system.diagnostics.stopwatch]::StartNew()
             $Solved = SolveSudoku -SudokuGrid $Grid -StopWatch $Timer
-            if ($Count -eq 25) {
-                break
+            If ($Count -eq 25) {
+                Break
             }
-            if (-not $Solved) {
-                continue
+            If (-not $Solved) {
+                Continue
                 $Count += 1
             }
-            break
+            Break
         }
         $Solved | should -be $true
     }
     It "Verifying Insane board solvable" {
         $Count = 0
         $Solved = $false
-        while($true) {
+        While($true) {
             $Grid = GenerateGrid -Difficulty "Insane"
             $Timer =  [system.diagnostics.stopwatch]::StartNew()
             $Solved = SolveSudoku -SudokuGrid $Grid -StopWatch $Timer
-            if ($Count -eq 25) {
-                break
+            If ($Count -eq 25) {
+                Break
             }
-            if (-not $Solved) {
-                continue
+            If (-not $Solved) {
+                Continue
                 $Count += 1
             }
-            break
+            Break
         }
         $Solved | should -be $true
     }
